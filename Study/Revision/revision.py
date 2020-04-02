d3e3668e30afbdc2312fe12827e0b07b6eb27fce
kimhg9511 <54794439+kimhg9511@users.noreply.github.com>
2020-04-02 17:05:49 +0900

    [2020-04-02]ver0.1(login,timeset)

---
+++ Study/Revision/revision.py
@@ -0,0 +1,21 @@
+from pywinauto import application
+from pywinauto import timings
+from pywinauto import findwindows
+import time
+import os
+
+app = application.Application()
+app.start("C:/KiwoomFlash3/bin/nkministarter.exe")
+
+title = "번개3 Login"
+dlg = timings.WaitUntilPasses(20, 0.5, lambda: app.window_(title=title))
+
+pass_ctrl = dlg.Edit2
+pass_ctrl.SetFocus()
+pass_ctrl.TypeKeys('empalxom')
+
+btn_ctrl = dlg.Button0
+btn_ctrl.Click()
+
+time.sleep(50)
+os.system("taskkill /im nkmini.exe")
